57abdedc0d852a34a7b3c22eea0d0ae72bcbfc2b
Kwon Hur <kwonhur1020@gmail.com>
2020-03-12 01:38:38 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [students, setStudents] = useState([]);
+
+  useEffect(()=>{
+    fetch("https://www.hatchways.io/api/assessment/students")
+      .then(response => response.json())
+      .then(json => setStudents(json.students.map((student)=>student={...student, ["open"]:false, ["tags"]:[]})))
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <ul className="list-group">
+
+          {students.map((student, index) =>
+          <div key={student.id}>
+          
+          <li className="list-group-item">
+          <i className="fa fa-plus" onClick={()=> setStudents(...students.slice(0,index), Object.assign(student,{"open": !student.open}), ...students.slice(index+1))}></i>
+          <img src={student.pic} className="rounded-circle border"></img>
+          <div className="list_item_text">
+          <h1>{student.firstName} {student.lastName}</h1>
+          <div className="sub_text">
+          <span>Email: {student.email}</span><br></br>
+          <span>Company: {student.company}</span><br></br>
+          <span>Skill: {student.skill}</span><br></br>
+          <span>Average: </span><br></br>
+          { student.open ?
+            <div id={student.id}>
+          {student.grades.map((grade, index) =>
+          <div key={index}>
+          <span>Test {index + 1}: {grade}</span><br></br>
+          </div>
+          )}
+          </div> : null }
+          
+          </div>
+          </div>
+          </li>
+          </div>
+          )}
+
+        </ul>
+      </div>
     </div>
   );
 }
